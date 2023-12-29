8bce4cf66eedbf1e4dea050e27caa357bc8284c3
Saami783 <sami.bahij1@gmail.com>
2023-12-29 22:35:58 +0100

    Ajout des fichiers sql

---
+++ sql/vehicule.sql
@@ -0,0 +1,18 @@
+INSERT INTO `vehicule` 
+(`id`, `modele`, `caution`, `nb_km`, `nb_place`, `couleur`, `annee`, `boite_vitesse`, `fuel`, `nb_cheveaux`, `is_disponible`, `etat`, `description`, `categorie_id`) 
+VALUES
+(1, 'SERIE 1', 2500, 32400, 5, 'Blanc', 2020, 'Automatique', 'Essence', 140, 0, 'parfait_etat', '', 7),
+(2, 'SERIE 2', 2500, 24546, 5, 'Gris', 2020, 'Automatique', 'Diesel', 150, 0, '', '', 7),
+(3, 'CLA 200', 2500, 12535, 5, 'Gris nardo', 2022, 'Automatique', 'Essence', 163, 1, '', '', 3),
+(4, 'CLA 250-E', 2500, 23445, 5, 'Noir', 2020, 'Automatique', 'Diesel', 150, 1, '', '', 3),
+(5, 'X4', 5000, 21423, 5, 'Noir', 2021, 'Automatique', 'Diesel', 190, 1, '', '', 7),
+(6, 'G63 AMG', 15000, 5843, 5, 'Noir', 2022, 'Automatique', 'Essence', 544, 1, '', '', 4),
+(7, 'C180', 2500, 53458, 5, 'Blanc', 2020, 'Automatique', 'Diesel', 122, 1, '', '', 3),
+(8, 'A180', 2500, 23084, 5, 'Blanc', 2020, 'Automatique', 'Essence', 136, 1, '', '', 3),
+(9, 'A200', 2500, 95438, 5, 'gris', 2020, 'Automatique', 'Essence', 163, 1, '', '', 3),
+(10, 'A1', 2000, 78345, 5, 'Gris nardo', 2020, 'Automatique', 'Essence', 150, 1, '', '', 1),
+(11, 'A3', 2500, 32048, 5, 'Bleu brillant', 2021, 'Automatique', 'Hybride', 150, 1, '', '', 1),
+(12, 'VELAR', 5000, 43583, 5, 'Noir brillant', 2018, 'Automatique', 'Diesel', 300, 1, '', '', 5),
+(13, '8', 2500, 23435, 5, 'Gris nardo', 2021, 'Automatique', 'Hybride', 150, 1, '', '', 6),
+(14, 'RLINE', 2000, 12059, 5, 'Bleu', 2022, 'Automatique', 'Essence', 110, 1, '', '', 6),
+(15, '595', 2000, 98547, 4, 'Gris', 2019, 'Automatique', 'Essence', 165, 1, '', '', 2);
