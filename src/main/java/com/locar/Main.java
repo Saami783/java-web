a88ff4c146d92a94a8be00bb59a906a9e821da77
HawaD29 <155446193+HawaD29@users.noreply.github.com>
2024-01-02 18:02:38 +0100

    Archi

--- src/main/java/com/locar/Main.java
+++ src/main/java/com/locar/Main.java
@@ -7,11 +7,16 @@ import com.locar.entities.Vehicule;
 import com.locar.services.VehiculeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import java.util.List;
 
@@ -36,6 +41,7 @@ public class Main {
         model.addAttribute("vehicules", vehicules);
         return "home";
     }
+
 
 
 }
--- src/main/java/com/locar/config/SpringSecurityConfig.java
+++ src/main/java/com/locar/config/SpringSecurityConfig.java
@@ -2,18 +2,9 @@ package com.locar.config;
 
 import com.locar.services.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SpringSecurityConfig {
 
     public static final String[] ENDPOINTS_WHITELIST = {
@@ -28,28 +19,6 @@ public class SpringSecurityConfig {
     @Autowired
     private UtilisateurService utilisateurService;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN");
-                    auth.requestMatchers("/user", "/reservations", "/reservations/{id}").hasRole("USER");
-                    auth.requestMatchers("/reservations/create", "paiement/**", "ckeckout/**").hasRole("USER");
-                    auth.requestMatchers("/register", "/", "/vehicules", "/vehicules/{id}", "/uploads/**").permitAll();
-                    auth.anyRequest().authenticated();
-                }).formLogin(Customizer.withDefaults()).build();
-    }
 
 
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(utilisateurService).passwordEncoder(bCryptPasswordEncoder);
-        return authenticationManagerBuilder.build();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
